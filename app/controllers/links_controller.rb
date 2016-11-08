cb6e40bf0c1011caf5c255bf5351fde151916f1d
Trent Whitinger <twhitinger@gmail.com>
2016-11-08 09:37:08 -0700

    add actions edit, update create, add actions destroy, remove invert use bang

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -11,27 +11,37 @@ class LinksController < ApplicationController
       flash[:success] = "Link to #{@link.title} added to your repo."
       redirect_to links_path
     else
-      flash[:warning] = "oops"
+      flash[:warning] = "Link must be valid format and all inputs filled in"
       redirect_to root_path
     end
   end
 
-    def update
-      @link = Link.find(params[:id])
-      @link.update_attributes(read: invert)
-      redirect_to links_path
-    end
+  def edit
+    @link = Link.find(params[:id])
+  end
 
-    private
-    def link_params
-      params.require(:link).permit(:url, :title, :user_id)
-    end
-
-    def invert
-      if @link.read
-        false
-      else
-        true
-      end
+  def update
+    @link = Link.find(params[:id])
+    if params[:link] && @link.update(link_params)
+      flash[:success] = "Updated your link"
+    elsif params[:link]
+      flash[:warning] = @link.errors.full_messages.join(', ')
+    else
+      @link.update_attributes(read: !@link.read)
+        render json: @link
     end
   end
+
+  def destroy
+    link = Link.where(user: current_user, id: params[:id])
+    link.destroy_all
+    redirect_to links_path
+  end
+
+  private
+
+  def link_params
+      params.require(:link).permit(:url, :title, :user_id, :tag_list)
+  end
+
+end
